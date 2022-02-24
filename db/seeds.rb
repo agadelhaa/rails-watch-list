e9ed716b1c61377661a810a22e447caee8113015
Amanda Amorim <amandagadelhaamorim@gmail.com>
2022-02-24 17:10:20 -0300

    watch-list funcional, com bookmarks completamente implementado

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+30.times do
+  title = Faker::Movie.title
+  overview = Faker::TvShows::RuPaul.quote
+  poster_url = "https://www.#{Faker::Games::WarhammerFantasy.location}.com"
+  rating = (rand(1.0..10.0)).round(1)
+
+  Movie.create(title: title, overview: overview, poster_url: poster_url, rating: rating)
+end
